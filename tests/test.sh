c30be6d2c9a4bff95826b129a65bc7c1988303c8
Amber Goulding <gouldinga1@cardiff.ac.uk>
2026-09-14 12:57:18 +0100

    Add test cases

--- tests/test.sh
+++ tests/test.sh
@@ -12,22 +12,30 @@ get_compiler_exit_code() {
 for test_path in tests/resources/*;
 do
   compiler_exit_code=$(<"${test_path}/${COMPILER_EXIT_CODE_FILE}")
-  exit_code=$(<"${test_path}/${EXIT_CODE_FILE}")
-  program_stdout=$(<"${test_path}/${PROGRAM}")
+  program_exit_code=$(<"${test_path}/${EXIT_CODE_FILE}")
+  program_stdout=$(<"${test_path}/${PROGRAM_STDOUT_FILE}")
   
   source_file_path="${test_path}/main.c"
   assembly_file_path="${test_path}/main.s"
   out_file_path="${test_path}/out"
 
-  ./hcc $source_file_path 2>&1 /dev/null
+  ./hcc $source_file_path >/dev/null 2>&1
   actual_compiler_exit_code=$?
-  if [[ $actual_compiler_exit_code -ne $compiler_exit_code ]]; then
-    echo "${test_path}: Compilation Returned ${actual_compiler_exit_code}, not ${compiler_exit_code}"
+  if [[ ($compiler_exit_code -eq 0 && $actual_compiler_exit_code -ne 0) ||
+        ($compiler_exit_code -ne 0 && $actual_compiler_exit_code -eq 0) ]]
+  then
+    echo "${test_path}: Unexpected compiler exit status"
     continue
   fi
 
-  ./assemble $assembly_file_path $out_file_path 2>&1 /dev/null
-  if [[ $? -ne "0" ]]; then
+  if [[ $compiler_exit_code -ne 0 ]]
+  then
+    continue
+  fi
+
+  ./assemble $assembly_file_path $out_file_path >/dev/null 2>&1
+  if [[ $? -ne "0" ]]
+  then
     echo "${test_path}: Assembly failed"
     continue
   fi
@@ -35,7 +43,8 @@ do
   actual_program_stdout=$(./${out_file_path})
   actual_program_exit_code=$?
 
-  if [[ $actual_program_stdout -ne $program_stdout ]]; then
+  if [[ $actual_program_stdout != $program_stdout ]]
+  then
     echo "${test_path}: Unexpected stdout"
     echo "got:"
     echo $actual_program_stdout
@@ -44,8 +53,9 @@ do
     continue
   fi
 
-  if [[ $actual_program_exit_code -ne $program_exit_code ]]; then
-    echo "${test_path}: Got exit code {program_exit_code}, got {actual_program_exit_code}"
+  if [[ $actual_program_exit_code -ne $program_exit_code ]]
+  then
+    echo "${test_path}: Got exit code ${program_exit_code}, got ${actual_program_exit_code}"
     continue
   fi
 done
