d062d5605d4a09eea0ac0302b6b9b7f6bc9d3fa2
Amber Goulding <gouldinga1@cardiff.ac.uk>
2026-09-15 17:38:47 +0100

    Lex more symbols

--- src/HCC/Lexer.hs
+++ src/HCC/Lexer.hs
@@ -20,7 +20,10 @@ data Token
   | OpenBrace
   | CloseBrace
   | Semicolon
+  | Plus
   | Negative
+  | Asterisk
+  | ForwardSlash
   | Tilde
   | Bang
   | Keyword Keyword
@@ -48,8 +51,17 @@ closeBraceParser = (\_ -> CloseBrace) <$> stringParser "}"
 semiColonParser :: LexerParser Token
 semiColonParser = (\_ -> Semicolon) <$> stringParser ";"
 
+plusParser :: LexerParser Token
+plusParser = (\_ -> Plus) <$> stringParser "+"
+
 negativeParser :: LexerParser Token
 negativeParser = (\_ -> Negative) <$> stringParser "-"
+
+asteriskParser :: LexerParser Token
+asteriskParser = (\_ -> Asterisk) <$> stringParser "*"
+
+forwardSlashParser :: LexerParser Token
+forwardSlashParser = (\_ -> ForwardSlash) <$> stringParser "/"
 
 tildeParser :: LexerParser Token
 tildeParser = (\_ -> Tilde) <$> stringParser "~"
@@ -64,7 +76,10 @@ grammarParser =
     <|> openBraceParser
     <|> closeBraceParser
     <|> semiColonParser
+    <|> plusParser
     <|> negativeParser
+    <|> asteriskParser
+    <|> forwardSlashParser
     <|> tildeParser
     <|> bangParser
 
